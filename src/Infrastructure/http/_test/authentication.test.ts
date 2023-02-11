847ff6ee47f976b9682dc2cab611f65b76633732
AR Project <alvenresta@gmail.com>
2023-02-11 22:31:50 +0700

    implement refreshToken route
    a little bit clean up

--- src/Infrastructure/http/_test/authentication.test.ts
+++ src/Infrastructure/http/_test/authentication.test.ts
@@ -19,6 +19,9 @@ describe('authentication endpoint', () => {
     await AuthenticationsTableTestHelper.cleanTable();
   });
   describe('when POST /authentications', () => {
+    /**
+     * FAIL TEST SECTION
+     */
     it('should fail and return 400 when no payload', async () => {
       const response = await request(server)
         .post('/authentications')
@@ -72,6 +75,9 @@ describe('authentication endpoint', () => {
       expect(response.body.message).toEqual('username dan password harus string');
     });
 
+    /**
+    * SUCCESS TEST SECTION
+    */
     it('should response 201 and return both accessToken and refreshToken ', async () => {
       // Arrange 
       const addUserPayload: IAddUserPayload = {
@@ -103,5 +109,133 @@ describe('authentication endpoint', () => {
       expect(loginResponse.body.data.refreshToken).toBeDefined()
       expect(totalRow).toBe(1)
       })
+  })
+
+  describe('PUT /authentications', () => {
+    /**
+    * FAIL TEST SECTION
+    */
+    it('should return fail, 400 when contain no payload', async () => {
+      const response = await request(server)
+        .put('/authentications')
+
+      expect(response.statusCode).toBe(400)
+      expect(response.body.status).toBeDefined()
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBeDefined()
+      expect(response.body.message).toBe('harus mengirimkan token refresh')
+    })
+
+    it('should response 400 if login payload not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        wrongProperty: 'wrong_data',
+      };
+      const response = await request(server)
+        .put('/authentications')
+        .send(requestPayload)
+        .set('Accept', 'application/json')
+
+ 
+      // Assert
+      expect(response.statusCode).toEqual(400);
+      expect(response.body.status).toBeDefined()
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBeDefined()
+      expect(response.body.message).toBe('harus mengirimkan token refresh')
+    })
+
+    it('should response 400 if payload have wrong data type', async () => {
+      // Arrange
+      const requestPayload = {
+        refreshToken: ['wrong data type'],
+      };
+      const response = await request(server)
+        .put('/authentications')
+        .send(requestPayload)
+        .set('Accept', 'application/json')
+
+ 
+      // Assert
+      expect(response.statusCode).toEqual(400);
+      expect(response.body.status).toBeDefined()
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBeDefined()
+      expect(response.body.message).toBe('refresh token harus string')
+    })
+
+    it('should response 400 and when token is invalid', async () => {
+      const addUserPayload: IAddUserPayload = {
+        username: 'arproject',
+        password: 'secret',
+        fullname: 'AR Project'
+      }
+      const loginPayload = {
+        username: 'arproject',
+        password: 'secret'
+      }
+
+      const registerResponse = await request(server)
+        .post('/users')
+        .send(addUserPayload)
+        .set('Accept', 'application/json')
+
+      const loginResponse = await request(server)
+        .post('/authentications')
+        .send(loginPayload)
+        .set('Accept', 'application/json')
+      
+      const { accessToken } = loginResponse.body.data
+
+      const refreshTokenResponse = await request(server)
+        .put('/authentications')
+        .send({refreshToken: accessToken})
+        .set('Accept', 'application/json')
+
+      expect(registerResponse.statusCode).toEqual(201)
+      expect(loginResponse.statusCode).toBe(201)
+      expect(refreshTokenResponse.statusCode).toEqual(400)
+      expect(refreshTokenResponse.body.status).toBe('fail')
+      expect(refreshTokenResponse.body.message).toBe('refresh token tidak valid')
+    })
+
+    /**
+    * SUCCESS TEST SECTION
+    */
+    it('should response 200 and return newAccessTokens', async () => {
+      const addUserPayload: IAddUserPayload = {
+        username: 'arproject',
+        password: 'secret',
+        fullname: 'AR Project'
+      }
+      const loginPayload = {
+        username: 'arproject',
+        password: 'secret'
+      }
+
+      const registerResponse = await request(server)
+        .post('/users')
+        .send(addUserPayload)
+        .set('Accept', 'application/json')
+
+      const loginResponse = await request(server)
+        .post('/authentications')
+        .send(loginPayload)
+        .set('Accept', 'application/json')
+      
+      const { refreshToken } = loginResponse.body.data
+
+      const refreshTokenResponse = await request(server)
+        .put('/authentications')
+        .send({refreshToken})
+        .set('Accept', 'application/json')
+
+      expect(registerResponse.statusCode).toEqual(201)
+      expect(loginResponse.statusCode).toBe(201)
+      expect(refreshTokenResponse.statusCode).toEqual(200)
+      expect(refreshTokenResponse.body.status).toBe('success')
+      expect(refreshTokenResponse.body.data.accessToken).toBeDefined()
+    })
+
   })
 })
