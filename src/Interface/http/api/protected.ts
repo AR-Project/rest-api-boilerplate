bfef80c1d1b926e1c9bf23ff3605b7b2d553d740
AR Project <alvenresta@gmail.com>
2023-02-15 22:59:53 +0700

    implement sanity check for expressjwt

--- src/Interface/http/api/protected.ts
+++ src/Interface/http/api/protected.ts
@@ -1,12 +1,26 @@
 import express, { type Request, type Response, type NextFunction } from 'express'
+import { expressjwt, Request as JWTRequest } from "express-jwt";
 
 const router = express.Router()
 
-router.get('/nonsecure',  (req: Request, res: Response, next: NextFunction) => {
+router.get('/nonsecure', (req: Request, res: Response, next: NextFunction) => {
   res.json({
     status: 'success',
     message: 'unprotected route'
   })
 })
+
+router.get(
+  '/secure',
+  (req: JWTRequest, res: Response) => {
+    res.json({
+      status: 'success',
+      message: 'protected route',
+      data: {
+        ...req.auth
+      }
+    })
+  }
+)
 
 export default router
