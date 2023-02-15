4abb0bc8bb2eb2fb1980679e4f4fb6e8c27339eb
AR Project <alvenresta@gmail.com>
2023-02-15 22:59:13 +0700

    implement token expiration

--- src/Infrastructure/security/JwtTokenManager.ts
+++ src/Infrastructure/security/JwtTokenManager.ts
@@ -1,17 +1,27 @@
-import AuthenticationTokenManager, { ITokenPayload } from '../../Applications/security/AuthenticationTokenManager.js'
-import InvariantError from '../../Commons/exceptions/InvariantError.js'
 import jwt from 'jsonwebtoken'
+import AuthenticationTokenManager, { ITokenPayload } from '../../Applications/security/AuthenticationTokenManager.js'
+
+import InvariantError from '../../Commons/exceptions/InvariantError.js'
 
 export default class JwtTokenManager extends AuthenticationTokenManager {
-  override async createAccessToken (payload: ITokenPayload): Promise<string> {
-    return jwt.sign(payload, process.env.ACCESS_TOKEN_KEY as string)
+  override async createAccessToken(payload: ITokenPayload): Promise<string> {
+    return jwt.sign(
+      payload,
+      process.env.ACCESS_TOKEN_KEY as string,
+      { expiresIn: `${process.env.ACCCESS_TOKEN_AGE}s` }
+    )
   }
 
-  override async createRefreshToken (payload: ITokenPayload): Promise<string> {
-    return jwt.sign(payload, process.env.REFRESH_TOKEN_KEY as string)
+  override async createRefreshToken(payload: ITokenPayload): Promise<string> {
+    return jwt.sign(
+      payload,
+      process.env.REFRESH_TOKEN_KEY as string,
+      { expiresIn: `${process.env.REFRESH_TOKEN_AGE}s` }
+
+    )
   }
 
-  override async verifyRefreshToken (token: string): Promise<ITokenPayload> {
+  override async verifyRefreshToken(token: string): Promise<ITokenPayload> {
     try {
       const artifact = jwt.verify(token, process.env.REFRESH_TOKEN_KEY as string) as any
       const payload: ITokenPayload = {
@@ -26,7 +36,7 @@ export default class JwtTokenManager extends AuthenticationTokenManager {
     }
   }
 
-  override async decodePayload (token: string): Promise<ITokenPayload> {
+  override async decodePayload(token: string): Promise<ITokenPayload> {
     const artifact = jwt.decode(token) as any
     const payload: ITokenPayload = {
       id: artifact.id,
